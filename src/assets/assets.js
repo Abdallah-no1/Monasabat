1c3628fabf69cf460d8d9ebd67c61449dd319262
Abdallah-no1 <79752203+Abdallah-no1@users.noreply.github.com>
2026-06-28 21:10:47 +0300

    Fix image dir

---
+++ src/assets/assets.js
@@ -0,0 +1,4 @@
+import logo from './logo.svg'
+import plainPic from './plane.jpg'
+
+export { logo, plainPic }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {logo} from "../assets/assets.js";
 
 function Navbar() {
     return (
         <nav className="dark:bg-black/50 absolute w-full rounded-b-xl flex justify-between items-center">
-            <img src="src/assets/logo.svg" alt="logo" className="w-50 m-2" />
+            <img src={logo} alt="logo" className="w-50 m-2" />
             <span className="text-xl text-white mr-4 font-bold">عبدالله ماهر الحاج عيد</span>
         </nav>
     );
